ae5d36efb79de3d92631131fa6177ba2c488f25f
Luiz Cantrella <luizcantrella@gmail.com>
2022-05-03 22:08:52 -0300

    feat(movimentacao): criar controller resource /correntistas com findAll e save

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -9,4 +9,19 @@ public class Conta {
   Long numero;
   @Column(name = "conta_saldo")
   Double saldo;
+  public Long getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Long numero) {
+    this.numero = numero;
+  }
+
+  public Double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(Double saldo) {
+    this.saldo = saldo;
+  }
 }
--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -16,4 +16,43 @@ public class Movimentacao {
   private Double valor;
   @Enumerated(EnumType.STRING)
   private MovimentacaoTipo tipo;
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Date getDataHora() {
+    return dataHora;
+  }
+
+  public void setDataHora(Date dataHora) {
+    this.dataHora = dataHora;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Double getValor() {
+    return valor;
+  }
+
+  public void setValor(Double valor) {
+    this.valor = valor;
+  }
+
+  public MovimentacaoTipo getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(MovimentacaoTipo tipo) {
+    this.tipo = tipo;
+  }
 }
